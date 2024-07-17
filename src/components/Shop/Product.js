8c35fe41211ad267f62d05d937462f415e9b18e5
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-07-17 16:58:01 +0330

    add and write some part of project

---
+++ src/components/Shop/Product.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class Product extends Component {
+    
+    render() {
+        return (
+            <div class="shop-item">
+                <span class="shop-item-title">Shirt</span>
+                <img class="shop-item-image" src='Images/shirt3.png' />
+                <div class="shop-item-details">
+                    <span class="shop-item-price">$129</span>
+                    <button
+                        class="btn btn-primary shop-item-button"
+                        type="button">
+                        ADD TO CART
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
